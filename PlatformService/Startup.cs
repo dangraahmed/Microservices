03f1445e1413c67277260f45867bd91b5670c4dc
Ahmed Amin Dangra <ahmed.dangra@northgateps.com>
2021-09-15 15:41:56 +0530

    SQL Migration related change

--- PlatformService/Startup.cs
+++ PlatformService/Startup.cs
@@ -19,16 +19,28 @@ namespace PlatformService
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public IConfiguration Configuration { get; }
+        private readonly IWebHostEnvironment _env;
+
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
         {
             Configuration = configuration;
+            _env = env;
         }
-
-        public IConfiguration Configuration { get; }
 
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("InMem"));
+            if (_env.IsProduction())
+            {
+                Console.WriteLine($"--> Using InMem DB");
+                services.AddDbContext<AppDbContext>(opt => opt.UseSqlServer(Configuration.GetConnectionString("PlatformsConn")));
+            }
+            else
+            {
+                Console.WriteLine($"--> Using InMem DB");
+                services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("InMem"));
+            }
+
             services.AddScoped<IPlatformRepo, PlatformRepo>();
             services.AddHttpClient<ICommandDataClient, HttpCommandDataClient>();
             services.AddControllers();
@@ -61,7 +73,7 @@ namespace PlatformService
                 endpoints.MapControllers();
             });
 
-            PrepDb.PrePolulation(app);
+            PrepDb.PrePolulation(app, env.IsProduction());
         }
     }
 }
